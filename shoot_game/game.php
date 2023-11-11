d55b15b50d67ee44d41a6e763623cbac4b1ef7d1
KrychleMarek <133642382+KrychleMarek@users.noreply.github.com>
2023-11-11 20:18:12 +0100

    Add files via upload

--- shoot_game/game.php
+++ shoot_game/game.php
@@ -8,12 +8,12 @@
   crossorigin="anonymous"></script>
     <script src="script.js"></script>
     </head>
-    <body onload="onLoad()">
+    <body onload="onLoad()" onload="main()" >
         <header><h1>Cowboy duel</h1></header>
     <div id="flex">
         <div id="GameControl">
             <h2>Nastavení</h2>
-            <button id="start" type="button" onclick="popisky();" >START</button>
+            <button id="start" type="button" onclick="popisky(), obstacles()" >START</button>
         </div>
         <div id="GameSpace">
             <div id="obst1"></div>
@@ -26,24 +26,111 @@
             <p id="pop1"> Player 1</p>
             <p id="pop2"> Player 2</p>
         </div>
-        <script>
-document.onkeydown = press;
-function press(e){
-    e=e || window.event;
-    if(e.keyCode == '39'){
-        movementRight();
+        <script>//input
+        var preMoveX;
+        var preMoveY;
+        var preMove = true;
+        {
+    var player1 = document.getElementById("p1");
+    var Keys = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+    };
+ window.onkeydown = function(e){
+        if(preMove){
+            preMoveX = player1.style.left;
+            preMoveY = player1.style.top;
+            preMove = false;
+        }
+        var kc = e.keyCode;
+         e.preventDefault();
+    
+         if(kc === 37) Keys.left = true;
+         if(kc === 38) Keys.up = true;
+         if(kc === 39) Keys.right = true;
+         if(kc === 40) Keys.down = true;
+     };
+     window.onkeyup = function(e){
+        preMove = true;  
+         var kc = e.keyCode;
+         e.preventDefault();
+    
+         if(kc === 37) Keys.left = false;
+         if(kc === 38) Keys.up = false;
+         if(kc === 39) Keys.right = false;
+         if(kc === 40) Keys.down = false;
+    };
+    function main(){
+        move();
     }
-    else if(e.keyCode == '37'){
-        movementLeft();
-    }
-    else if(e.keyCode == '38'){
+ function move(){
+    if(Keys.up){
         movementUp();
-    }else if(e.keyCode = '40'){
+    }
+    
+    if(Keys.down){
         movementDown();
     }
-}
+    
+    if(Keys.left) {
+        movementLeft();
+    }
+    
+    if(Keys.right){
+        movementRight();
+    }
 
-</script>
+    if(Keys.up&&Keys.right||Keys.up&&Keys.left||Keys.down&&Keys.right||Keys.down&&Keys.left){
+        diagMovement();
+    }
+}
+setInterval(function(){
+    collision();
+    move();
+}, 100);}
+        </script>
+
+        <script>//Obstacles
+            var Obst1 = {
+                x1: $('#obst1').position().left,//levá
+                x2: $('#obst1').position().left+$("#obst1").width(), //pravá
+                y1: $('#obst1').position().top,//horní
+                y2: $('#obst1').position().top+$("#obst1").height() //spodní
+            };
+            var Obst2 = {
+                x1: $('#obst2').position().left,
+                x2: $('#obst2').position().left+$("#obst2").width(),
+                y1: $('#obst2').position().top,
+                y2: $('#obst2').position().top+$("#obst2").height()
+            };
+            var Obst3 = {
+                x1: $('#obst3').position().left,
+                x2: $('#obst3').position().left+$("#obst3").width(),
+                y1: $('#obst3').position().top,
+                y2: $('#obst3').position().top+$("#obst3").height()
+            };
+            var Obst4 = {
+                x1: $('#obst4').position().left,
+                x2: $('#obst4').position().left+$("#obst4").width(),
+                y1: $('#obst4').position().top,
+                y2: $('#obst4').position().top+$("#obst4").height()
+            };
+            var Obst5 = {
+                x1: $('#obst5').position().left,
+                x2: $('#obst5').position().left+$("#obst5").width(),
+                y1: $('#obst5').position().top,
+                y2: $('#obst5').position().top+$("#obst5").height()
+            };
+            var Obst = [];
+            Obst.push(Obst1, Obst2, Obst3, Obst4, Obst5);
+            console.log(Obst1);
+            console.log(Obst2);
+            console.log(Obst3);
+            console.log(Obst4);
+            console.log(Obst5);
+        </script>
     </div>
     </body>
     <footer>
--- shoot_game/script.js
+++ shoot_game/script.js
@@ -1,6 +1,5 @@
 var y1, x1, y2, x2;
-var boundY, boundX, position;
-
+var preMoveX, preMoveY;
 function onLoad(){
     y1 = $("#p1").css("top");
     x1 = $("#p1").css("left");
@@ -10,11 +9,6 @@ function onLoad(){
     x1 = Number(x1.substring(0,x1.length-2));
     y2 = Number(y2.substring(0,y2.length-2));
     x2 = Number(x2.substring(0,x2.length-2));
-
-    position = $("#GameSpace").position();
-    boundY = position.top;
-    boundX = position.left;
-    var obs1 = $("#obst1").css("top");
 }
 
 
@@ -32,36 +26,67 @@ function popisky(){
 }
 
 
+{//PLAYER1
+
+
 function movementRight(){
-    if(x1<1380){
+    if(x1<1370&&Object.values(Keys).reduce((a, item) => a + item, 0)==1){
     var player1 = document.getElementById("p1");
-    $(player1).stop().animate({"left":(x1=x1+15)},35);
+    $(player1).stop().animate({"left":(x1=x1+20)},80);
     console.log("Pravá");
     return false;}
         else return false;
 }
 
 function movementLeft(){
-    if(x1>0){
+    if(x1>10&&Object.values(Keys).reduce((a, item) => a + item, 0)==1){
     var player1 = document.getElementById("p1");
-    $(player1).stop().animate({"left":(x1=x1-15)},35);
+    $(player1).stop().animate({"left":(x1=x1-20)},80);
     console.log("Levá");
     return false;}
     else return false;
 }
 function movementUp(){
-    if(y1>240){
+    if(y1>240&&Object.values(Keys).reduce((a, item) => a + item, 0)==1){
     var player1 = document.getElementById("p1");
-    $(player1).stop().animate({"top":(y1=y1-15)},35);
+    $(player1).stop().animate({"top":(y1=y1-20)},80);
     console.log("Nahoru");
     return false;}
     else return false;
 }
 function movementDown(){
-    if(y1<735){
+    if(y1<730&&Object.values(Keys).reduce((a, item) => a + item, 0)==1){
     var player1 = document.getElementById("p1");
-    $(player1).stop().animate({"top":(y1=y1+15)},35);
+    $(player1).stop().animate({"top":(y1=y1+20)},80);
     console.log("Dolů");
     return false;}
     else return false;
+}
+function diagMovement(){
+    var player1 = document.getElementById("p1");
+    if(Keys.up && Keys.right && y1>240 && x1<1370){
+        $(player1).stop().animate({"top":(y1=y1-20),"left":(x1=x1+20)},80);
+    }else if(Keys.up && Keys.left && y1>240 && x1>10){
+        $(player1).stop().animate({"top":(y1=y1-20),"left":(x1=x1-20)},80);
+    }else if(Keys.down && Keys.right && y1<730 && x1<1370){
+        $(player1).stop().animate({"top":(y1=y1+20),"left":(x1=x1+20)},80);
+    }else if(Keys.down && Keys.left && y1<730 && x1>10){
+        $(player1).stop().animate({"top":(y1=y1+20),"left":(x1=x1-20)},80);
+    }
+}
+}
+
+{//PLAYER2
+
+}
+
+{//OBSTACLES
+function collision(){
+    var player1 = document.getElementById("p1");
+for(let i = 0; i<Obst.length; i++){
+    if(Obst[i].x1<=x1&&Obst[i].x2>=x1&&Obst[i].y1<=y1&&Obst[i].y2>=y1){
+        $(player1).stop().animate({"top":(preMoveY),"left":(preMoveX)},0);
+    }
+  }
+}
 }
